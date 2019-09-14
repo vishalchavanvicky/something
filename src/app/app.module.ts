84d2073b6be77defd09400dbd052b9f836e58b95
Vishal <vishalchavanvicky@gmail.com>
2019-09-14 09:06:28 +0530

    nav Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,MatSidenavModule,MatToolbarModule,MatIconModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    LayoutModule,
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
