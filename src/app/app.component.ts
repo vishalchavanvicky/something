33422e924f8bd66881d18003cc4232dca7bdb4bf
Vishal <vishalchavanvicky@gmail.com>
2019-09-15 16:07:32 +0530

    first Home page complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'something';
   opened=false;
+  constructor(private router:Router)  { }
+
+  ngOnInit(){
+    this.goto("");
+  }
+
+  goto(value){
+      this.router.navigate([value])
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,17 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,MatSidenavModule,MatToolbarModule,MatIconModule,
